a00c19c67302dbb524f600b624ee93a4c1ee1953
Jamie <Jamie@Jamies-MacBook-Air.local>
2022-04-09 22:02:29 -0400

    added dropdown box to view credit/debit

--- src/components/clock.js
+++ src/components/clock.js
@@ -8,7 +8,7 @@ import {Component} from 'react';
 class Clock extends Component {
     constructor(props) {
       super(props);
-      this.state = {date: new Date()};
+      this.state = {date: new Date() ,firstName: '',showName: false, userName: '' };
     }
   
     componentDidMount() {
@@ -26,22 +26,50 @@ class Clock extends Component {
         date: new Date()
       });
     }
+
+  
+    displayNameHandler = (e) => {
+      let updatedName = e.target.value;
+      this.setState({ firstName: updatedName });
+    }
+  
+    handleSubmit = (e) => {
+      e.preventDefault();
+      this.setState({
+        showName: true,
+        userName: this.state.firstName
+      });
+    }
     
     //what is actually going to print out to the screen !
     render() {
       return (
         <div>
-          <h1>Hello ... Welcome to Bank of React!</h1>
+          <h1>Hello {this.state.showName && <span> {this.state.userName}</span>}  welcome to Bank of React!</h1>
           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
 
-
-          <h3>Customize your profile</h3>
-          <h4>Update your username below ! </h4>
-          <input type="text" value={this.state.value} onChange={this.handleChange}/> 
-          <input type="submit" value="Submit" /> 
+          <h3>Update your username & Customize your profile below ! </h3>
           
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <label>Enter Username</label>
+              <input type="text" name="firstName" onChange={this.displayNameHandler} value={this.state.firstName} />
+              <button type="submit" onClick={this.handleSubmit}>Submit</button>
+              
+          </form>
+          </div>
+
+          <h4>Select an option to view Credits or Debits</h4>
+          <select value={this.state.selectValue} onChange={this.handleChange}>
+            <option value="Credits">Credits</option>
+            <option value="Debits">Debits</option>
+          </select>
           
         </div>
+          
+          
+          
+        
       );
     }
   }
