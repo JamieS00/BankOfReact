ac6d8a7d69dc4a77134f81c80ef8094e8833755c
Jamie <Jamie@Jamies-MacBook-Air.local>
2022-04-12 10:32:16 -0400

    added code to display crdits/debits

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Component} from 'react';
-//import {TextInput} from 'react'
+import axios from 'axios';
 
 
 // just class def & you shuld hve only 1 render call
@@ -8,13 +8,16 @@ import {Component} from 'react';
 class Clock extends Component {
     constructor(props) {
       super(props);
-      this.state = {date: new Date() ,firstName: '',showName: false, userName: '' };
+      this.state = {date: new Date() ,firstName: '',showName: false, userName: '', selectValue: '',
+      data: '' };
     }
   
     componentDidMount() {
       this.timerID = setInterval(
         () => this.tick(),1000 //1000 ms, which is in seconds
       );
+
+      
     }
   
     componentWillUnmount() {
@@ -40,6 +43,40 @@ class Clock extends Component {
         userName: this.state.firstName
       });
     }
+
+    // handleSelect = (e) =>{
+    //   this.setState({selectValue:e.target.value});
+    //   if (e.target.value == 'Debits'){
+    //     React.useEffect(() => {
+    //       axios.get('https://moj-api.herokuapp.com/debits').then((response) => { 
+    //         this.setState({data:response.data});
+            
+    //       });
+    //     }, []);
+        
+        
+    //   } 
+    //   else if (e.target.value == 'Credits'){
+    //     React.useEffect(() => {
+    //       axios.get('https://moj-api.herokuapp.com/credits').then((response) => { 
+    //         this.setState({data:response.data});
+            
+    //       });
+    //     }, []);
+        
+
+    //   }
+    // }
+
+    //not sure 
+    async componentDidMount(){
+      let response = await axios.get('https://moj-api.herokuapp.com/debits')
+      let debits = response.data;
+      this.setState({debits: debits});
+    }
+
+  
+
     
     //what is actually going to print out to the screen !
     render() {
@@ -60,10 +97,12 @@ class Clock extends Component {
           </div>
 
           <h4>Select an option to view Credits or Debits</h4>
-          <select value={this.state.selectValue} onChange={this.handleChange}>
+          <select value={this.state.selectValue} onChange={this.handleSelect}>
             <option value="Credits">Credits</option>
             <option value="Debits">Debits</option>
           </select>
+
+          <p>{this.state.data}</p>
           
         </div>
           
