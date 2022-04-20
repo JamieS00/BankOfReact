6932432e02a4d7af33eeccac9a7319797f1ac59b
Jamie <Jamie@Jamies-MacBook-Air.local>
2022-04-19 20:48:29 -0400

    code to display debits

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,7 +1,7 @@
 //import React from 'react';
 import {Component} from 'react';
 import axios from 'axios';
-import React,{useState} from 'react'
+// import React,{useState} from 'react'
 
 
 // just class def & you shuld hve only 1 render call
@@ -10,9 +10,9 @@ class Clock extends Component {
     constructor(props) {
       super(props);
       this.state = {date: new Date() ,firstName: '',showName: false, userName: '', selectValue: '',
-      data: '',bg_color: '', tempbg_color:'',textColor:'', temptext_color:'',click: true };
+      data: '',bg_color: '', tempbg_color:'',textColor:'', temptext_color:'',click: true, 
+      selected: "debits",debits:[],credits:[], };
     }
-
 
   
     componentDidMount() {
@@ -70,35 +70,45 @@ class Clock extends Component {
       
     }
 
-    handleSelect = (e) =>{
-      this.setState({selectValue:e.target.value});
-      if (e.target.value == 'Debits'){
-        React.useEffect(() => {
-          axios.get('https://moj-api.herokuapp.com/debits').then((response) => { 
-            this.setState({data:response.data});
-            console.log(response.data)
+    // handleSelect = (e) =>{
+    //   this.setState({selectValue:e.target.value});
+    //   if (e.target.value == 'Debits'){
+    //     React.useEffect(() => {
+    //       axios.get('https://moj-api.herokuapp.com/debits').then((response) => { 
+    //         this.setState({data:response.data});
+    //         console.log(response.data)
             
-          });
-        }, []);
+    //       });
+    //     }, []);
         
-        
-      } 
-      else if (e.target.value == 'Credits'){
-        React.useEffect(() => {
-          axios.get('https://moj-api.herokuapp.com/credits').then((response) => { 
-            this.setState({data:response.data});
-            console.log(response.data)
+    //   } 
+    //   // else if (e.target.value == 'Credits')
+    //   else{
+    //     React.useEffect(() => {
+    //       axios.get('https://moj-api.herokuapp.com/credits').then((response) => { 
+    //         this.setState({data:response.data});
+    //         console.log(response.data)
             
-          });
-        }, []);
-        
+    //       });
+    //     }, []);
+    //   }
+      
+      async componentDidMount() {
+        let response = await axios.get("https://moj-api.herokuapp.com/debits");
+        let debits = response.data;
+        this.setState({debits: debits});
+      }  
 
+      displayDebits = () => {
+        const { debits } = this.state;
+        return debits.map((debit) => {
+            let date = debit.date.slice(0,10);
+            return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+        }) 
       }
-
-
-   
-
-    }
+      displayCredit = () =>{
+        return <h2>credits</h2>;
+      }
   
 
     
@@ -111,21 +121,26 @@ class Clock extends Component {
           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
 
         
-          {/* Credit/Debit drop down list*/}
+          {/*--------------Credit/Debit drop down list----------------------------*/}
           <h4>Select an option to view Credits or Debits</h4>
-          <select value={this.state.selectValue} onChange={this.handleSelect}>
+          <select value={this.handleChange} onChange={this.handleSelect}>
             <option value="Credits">Credits</option>
             <option value="Debits">Debits</option>
           </select>
 
           <p>{this.state.data}</p>
 
+          if(this.state.selected === "debits"){
+             return (<div>{this.displayDebits()}</div>);
+          }
+            return <div>{this.displayCredit()}</div>;
+          }
+
+          {/*--------------Credit/Debit drop down list----------------------------*/}
 
 
 
-          {/* Profile button - needs to display the form (where users enters name,
-          enter a hex value for background color and text color& this form should dissappear once submitted*/}
-          
+          {/*------------------Profile button - that allows you to eneter username, hex bg & tect color*/}
           <button onClick={this.handleClickOnCustomize}>CUSTOMIZE YOUR PROFILE</button>
         
           {/*Username*/}
@@ -159,7 +174,9 @@ class Clock extends Component {
             </form>
          </div>}
          </div>
+         {/*------------------Profile button - that allows you to eneter username, hex bg & tect color*/}
 
+            {/* ----------------------------SUBMIT BUTTTON----------------------------*/}
           <div>
           {!this.state.click &&<div>
           <button type="submit" onClick={this.handleSubmit}>SUBMIT</button>
